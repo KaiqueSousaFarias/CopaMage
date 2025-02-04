edc4af0ccc94ac8b2a44344ca575e224ab5cb8ce
Kaique Sousa <63821804+KaiqueSousaFarias@users.noreply.github.com>
2025-02-04 13:31:47 -0300

    fix: Corrigindo a correção 😧

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin-ext"] })
 
@@ -8,9 +9,6 @@ export const metadata: Metadata = {
   title: "5ª Copa Magé de Jiu-Jitsu",
   description: "O melhor evento de lutas casadas, repleto de novidades!",
   icons: "/hazaq.png",
-  other: {
-    "google-adsense-account": "ca-pub-7401469931656779"
-  }
 }
 
 export default function RootLayout({
@@ -20,9 +18,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-7401469931656779" />
+      </head>
       <body className={inter.className}>
         {children}
       </body>
+      <Script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7401469931656779"
+          strategy="afterInteractive"
+          crossOrigin="anonymous"
+        />
     </html>
   )
 }
