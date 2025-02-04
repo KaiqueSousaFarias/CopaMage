48e122c9cb1069fe0056c72c6cfbe2aa8010b8d5
Kaique Sousa <63821804+KaiqueSousaFarias@users.noreply.github.com>
2025-02-04 14:13:32 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <AdSense pId="ca-pub-7401469931656779" />
+        <AdSense pId="7401469931656779" />
       </head>
       <body className={inter.className}>
         {children}
