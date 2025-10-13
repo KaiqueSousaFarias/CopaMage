6d349fb4330c51d933fc63d6cfc3cddf8f74a7a8
Kaique Sousa <63821804+KaiqueSousaFarias@users.noreply.github.com>
2025-10-13 20:27:57 -0300

    feat: Adicionar suporte ao Google AdSense e criar arquivo ads.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,39 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Analytics } from '@vercel/analytics/next'
+import type {Metadata} from 'next'
+import {GeistSans} from 'geist/font/sans'
+import {GeistMono} from 'geist/font/mono'
+import {Analytics} from '@vercel/analytics/next'
 import './globals.css'
+import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: 'Copa Magé',
-  description: 'Copa Magé - Evento de Jiu-Jitsu em Magé, RJ',
-  generator: 'Copa Magé',
-  icons: {
-    icon: '/image/hazaq-logo.png'
-  }
+    title: 'Copa Magé',
+    description: 'Copa Magé - Evento de Jiu-Jitsu em Magé, RJ',
+    generator: 'Copa Magé',
+    icons: {
+        icon: '/image/hazaq-logo.png'
+    }
 }
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="pt-BR">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+    return (
+        <html lang="pt-BR">
+        <head>
+            {/* Script do AdSense para Auto Ads */}
+            <Script
+                async
+                src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${process.env.ADSENSE_CLIENT_ID}`}
+                strategy="afterInteractive" // Carrega após interatividade
+                crossOrigin="anonymous"
+            />
+        </head>
+        <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
-        <Analytics />
-      </body>
-    </html>
-  )
+        <Analytics/>
+        </body>
+        </html>
+    )
 }
